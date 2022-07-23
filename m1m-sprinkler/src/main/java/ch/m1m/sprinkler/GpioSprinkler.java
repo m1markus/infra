0b755b8de481636c05b5bbf4b3dc693b0416f891
mmm <mmm7@bluewin.ch>
2022-07-23 22:29:49 +0200

    sprinkler: Call gpio.shutdown() on application CDI ShutdownEvent

--- m1m-sprinkler/src/main/java/ch/m1m/sprinkler/GpioSprinkler.java
+++ m1m-sprinkler/src/main/java/ch/m1m/sprinkler/GpioSprinkler.java
@@ -1,10 +1,12 @@
 package ch.m1m.sprinkler;
 
 import com.pi4j.io.gpio.*;
+import io.quarkus.runtime.ShutdownEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
 
 // GpioController gpioController = GpioFactory.getInstance();
 //
@@ -33,6 +35,20 @@ public class GpioSprinkler {
             LOG.error("failed to initialize GPIO");
             LOG.error("when running on RaspberryPi this is critical");
             LOG.warn("falling back to GPIO emulation mode");
+        }
+    }
+
+    void onStop(@Observes ShutdownEvent event) throws InterruptedException {
+        LOG.info("sprinkler onStop()...");
+        shutdown();
+    }
+
+    private void shutdown() {
+        if (isOnRealPi) {
+            LOG.info("calling gpio.shutdown()");
+            gpio.shutdown();
+        } else {
+            LOG.info("fake gpio.shutdown()");
         }
     }
 
