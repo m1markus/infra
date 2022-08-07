102c27432d1546495ec5c9669ed1060181b85fb2
mmm <mmm7@bluewin.ch>
2022-08-07 22:18:42 +0200

    sprinkler: Add configuration with system property

--- m1m-sprinkler/src/main/java/ch/m1m/sprinkler/AppConfig.java
+++ m1m-sprinkler/src/main/java/ch/m1m/sprinkler/AppConfig.java
@@ -1,9 +1,14 @@
 package ch.m1m.sprinkler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class AppConfig {
+
+    private static Logger LOG = LoggerFactory.getLogger(AppConfig.class);
 
     private String dataDirectory;
 
@@ -12,6 +17,7 @@ public class AppConfig {
     }
 
     public void setDataDirectory(String dataDirectory) {
+        LOG.info("setting DataDirectory to: {}", dataDirectory);
         this.dataDirectory = dataDirectory;
     }
 }
--- m1m-sprinkler/src/main/java/ch/m1m/sprinkler/AppLifecycleBean.java
+++ m1m-sprinkler/src/main/java/ch/m1m/sprinkler/AppLifecycleBean.java
@@ -6,23 +6,35 @@ import javax.enterprise.event.Observes;
 import io.quarkus.runtime.ShutdownEvent;
 import io.quarkus.runtime.StartupEvent;
 
+import org.eclipse.microprofile.config.ConfigProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @ApplicationScoped
 public class AppLifecycleBean {
 
+    private static final String SPRINKLER_DATA_DIR = "sprinkler.data.directory";
+
     private static Logger LOG = LoggerFactory.getLogger(AppLifecycleBean.class);
 
     void onStart(@Observes StartupEvent event, AppConfig appConfig) {
 
-        appConfig.setDataDirectory("/tmp");
-
         LOG.info("The application is starting...");
+
+        appConfig.setDataDirectory(getConfigValue(SPRINKLER_DATA_DIR));
+
         LOG.info("config DataDirectory is {}", appConfig.getDataDirectory());
     }
 
     void onStop(@Observes ShutdownEvent event) {
         LOG.info("The application is stopping...");
+    }
+
+    private String getConfigValue(String keyName) {
+        String value = System.getProperty(keyName);
+        if (value == null) {
+            value = ConfigProvider.getConfig().getValue(keyName, String.class);
+        }
+        return value;
     }
 }
